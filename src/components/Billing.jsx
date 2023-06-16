1c3ba494a68da2d43031010dd83fb46542d98f3d
adnanfathino16 <fathinoadnan@gmail.com>
2023-06-16 13:09:33 +0700

    update billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -2,7 +2,7 @@ import { apple, bill, google } from "../assets";
 import styles, { layout } from "../style";
 
 const Billing = () => (
-  <section id="prouduct" className={layout.sectionReverse}>
+  <section id="product" className={layout.sectionReverse}>
     <div className={layout.sectionImgReverse}>
       <img src={bill} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
